0066e531dbd130af2078854aa7de8a962c23b92c
Chris Rudmin <chris.rudmin@gmail.com>
2019-04-27 02:36:00 -0400

    Remove content type

--- src/services/MoodeCommand.js
+++ src/services/MoodeCommand.js
@@ -2,17 +2,12 @@ import axios from 'axios';
 import { MoodeDomain } from '../config/AppConstants';
 
 const url = cmd => `${MoodeDomain}/command/moode.php?cmd=${cmd}`;
-const config = {
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded'
-  }
-};
 
 class MoodeCommand {
   static clearPlayAll(tracks) {
     const formData = new FormData();
     tracks.forEach(file => formData.append('path[]', file));
-    return axios.post(url('clrplayall'), formData, config);
+    return axios.post(url('clrplayall'), formData);
   }
 
   static loadLib() {
