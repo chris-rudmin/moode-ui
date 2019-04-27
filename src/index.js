eccfa285c7a44b3552a7a60d316afaaea305f3ee
Chris Rudmin <chris.rudmin@gmail.com>
2019-04-27 01:30:12 -0400

    Lint fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import AlbumGrid from './components/AlbumGrid';
-import CssBaseline from '@material-ui/core/CssBaseline'
 
 ReactDOM.render(
   <>
