307ea25847b2918c420ab2ac16a8f533c041f0a0
Chris Rudmin <chris.rudmin@gmail.com>
2019-05-31 10:31:39 -0400

    WIP add virtualization

--- src/components/AlbumGrid.js
+++ src/components/AlbumGrid.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Measure from 'react-measure';
@@ -7,6 +7,7 @@ import MoodeCommand from '../services/MoodeCommand';
 import Library from '../services/Library';
 import { cardMaxWidth, cardMargin } from '../config/AppConstants';
 import Loading from './Loading';
+import { useInView } from 'react-intersection-observer'
 
 const totalMargin = cardMargin * 2;
 const styles = () => ({
@@ -14,37 +15,37 @@ const styles = () => ({
     padding: 40
   },
   cardCluster: {
-    '&[data-num-cols="2"] > div': {
+    '&[data-col-count="2"] > div': {
       width: `calc((100%/2) - ${totalMargin}px)`
     },
-    '&[data-num-cols="3"] > div': {
+    '&[data-col-count="3"] > div': {
       width: `calc((100%/3) - ${totalMargin}px)`
     },
-    '&[data-num-cols="4"] > div': {
+    '&[data-col-count="4"] > div': {
       width: `calc((100%/4) - ${totalMargin}px)`
     },
-    '&[data-num-cols="5"] > div': {
+    '&[data-col-count="5"] > div': {
       width: `calc((100%/5) - ${totalMargin}px)`
     },
-    '&[data-num-cols="6"] > div': {
+    '&[data-col-count="6"] > div': {
       width: `calc((100%/6) - ${totalMargin}px)`
     },
-    '&[data-num-cols="7"] > div': {
+    '&[data-col-count="7"] > div': {
       width: `calc((100%/7) - ${totalMargin}px)`
     },
-    '&[data-num-cols="8"] > div': {
+    '&[data-col-count="8"] > div': {
       width: `calc((100%/8) - ${totalMargin}px)`
     },
-    '&[data-num-cols="9"] > div': {
+    '&[data-col-count="9"] > div': {
       width: `calc((100%/9) - ${totalMargin}px)`
     },
-    '&[data-num-cols="10"] > div': {
+    '&[data-col-count="10"] > div': {
       width: `calc((100%/10) - ${totalMargin}px)`
     },
-    '&[data-num-cols="11"] > div': {
+    '&[data-col-count="11"] > div': {
       width: `calc((100%/11) - ${totalMargin}px)`
     },
-    '&[data-num-cols="12"] > div': {
+    '&[data-col-count="12"] > div': {
       width: `calc((100%/12) - ${totalMargin}px)`
     }
   }
@@ -56,8 +57,9 @@ class AlbumGrid extends Component {
     this.state = {
       albums: [],
       isLoading: false,
-      progress: 0,
-      numCols: 2
+      colCount: 0,
+      rowCount: 0,
+      rowHeight: 0,
     };
   }
 
@@ -76,40 +78,45 @@ class AlbumGrid extends Component {
     });
   }
 
-  static getNumCols(width) {
-    return Math.ceil(width / cardMaxWidth);
-  }
-
   render() {
     const { classes } = this.props;
     const albumCards = this.state.albums.map(album => (
-      <AlbumCard key={album.albumKey} album={album} />
+      <AlbumCard key={album.albumKey} album={album}/>
     ));
 
     return this.state.isLoading ? (
       <Loading />
     ) : (
-      <div className={classes.root}>
-        <Measure
-          bounds
-          onResize={contentRect => {
-            this.setState(prevState => ({
-              ...prevState,
-              numCols: AlbumGrid.getNumCols(contentRect.bounds.width)
-            }));
-          }}
-        >
-          {({ measureRef }) => (
+      <Measure
+        bounds
+        onResize={({ bounds }) => {
+          const colCount = Math.ceil(bounds.width / cardMaxWidth);
+          const rowCount = Math.ceil(this.state.albums.length / colCount);
+          const rowHeight = (bounds.width / colCount) + 68;
+          const viewportHeight = rowHeight * 8;
+          const totalHeight = rowHeight * rowCount;
+          this.setState(prevState => ({
+            ...prevState,
+            colCount,
+            rowCount,
+            rowHeight,
+          }));
+        }}
+      >
+        {({ measureRef }) => (
+          <div className={classes.root}>
+            <div/>
             <div
               ref={measureRef}
               className={classes.cardCluster}
-              data-num-cols={this.state.numCols}
+              data-col-count={this.state.colCount}
             >
-              {albumCards}
+              {albumCards.slice(0,this.state.colCount*10)}
             </div>
-          )}
-        </Measure>
-      </div>
+            <div style={{height: this.state.rowHeight*this.state.rowCount}} />
+          </div>
+        )}
+      </Measure>
     );
   }
 }
